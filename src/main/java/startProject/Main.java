6ebae0aa7ca2bccff538ac740299c1f1f1549a42
Hovhannisyan Hayk <hovhannisyanhayk56@gmail.com>
2020-07-14 11:45:33 -0700

    final final changes

--- src/main/java/startProject/Main.java
+++ src/main/java/startProject/Main.java
@@ -5,25 +5,22 @@ public class Main {
 	public static void main(String[] args) {
 		Start start = new Start();
 		Database database = new Database();
-		
+
 		start.enterYourEmail();
 		start.checkEmailValidation();
 		database.checkEmailInDatabase();
-		start.enterYourPassword();
-		start.checkPasswordValidation();
-		database.checkPasswordInDatabase();
-		
+		// start.enterYourPassword();
+		// start.checkPasswordValidation();
+		// database.checkPasswordInDatabase();
 
-	   /* Set<Entry<String, String>> st = database.hm.entrySet();
-
-		for (Map.Entry<String, String> me : st) {
-			System.out.print(me.getKey() + " - ");
-			System.out.println(me.getValue());
-		}
-		System.out.println();
-		database.checkEmailInDatabase();
-		database.checkPasswordInDatabase();
-
-	}*/
-	}		
+		/*
+		 * Set<Entry<String, String>> st = database.hm.entrySet();
+		 * 
+		 * for (Map.Entry<String, String> me : st) { System.out.print(me.getKey() +
+		 * " - "); System.out.println(me.getValue()); } System.out.println();
+		 * database.checkEmailInDatabase(); database.checkPasswordInDatabase();
+		 * 
+		 * }
+		 */
+	}
 }
--- src/main/java/startProject/Start.java
+++ src/main/java/startProject/Start.java
@@ -11,16 +11,16 @@ public class Start {
 
 	Scanner in = new Scanner(System.in);
 
+	public String regexEmail = "^[A-Z0-9._%+-]+@[A-Z]+.[A-Z]";
+	public final Pattern VALID_EMAIL_ADDRESS_REGEX = Pattern.compile(regexEmail, Pattern.CASE_INSENSITIVE);
+	Matcher matcherEmail = VALID_EMAIL_ADDRESS_REGEX.matcher("email");
+	boolean boolEmail = matcherEmail.matches();
+
 	public void enterYourEmail() {
 		System.out.print("Enter your email  -- ");
 		email = in.nextLine();
 
 	}
-
-	public final Pattern VALID_EMAIL_ADDRESS_REGEX = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z]+.[A-Z]",
-			Pattern.CASE_INSENSITIVE);
-	Matcher matcherEmail = VALID_EMAIL_ADDRESS_REGEX.matcher("start.email");
-	boolean boolEmail = matcherEmail.matches();
 
 	public void checkEmailValidation() {
 		if (email.isEmpty()) {
@@ -30,15 +30,15 @@ public class Start {
 		}
 		if (boolEmail) {
 			System.out.println();
-		} else {
-			System.out.println("your email is invalid ");
-			enterYourEmail();
-			checkEmailValidation();
 		}
+		System.out.println("your email is invalid ");
+		enterYourEmail();
+		checkEmailValidation();
 	}
 
-	public final Pattern VALID_PASSWORD_REGEX = Pattern.compile("^[A-Z]+[0-9]+[a-z]+[.@$%+_]");
-	Matcher matcherPassword = VALID_PASSWORD_REGEX.matcher("start.password");
+	public String regexPassword = "[A-Za-z0-9-_+@%]";
+	public Pattern VALID_PASSWORD_REGEX = Pattern.compile(regexPassword);
+	Matcher matcherPassword = VALID_PASSWORD_REGEX.matcher("password");
 	boolean boolPassword = matcherPassword.matches();
 
 	public void enterYourPassword() {
@@ -51,25 +51,25 @@ public class Start {
 			System.out.println("your password is empty  ");
 			enterYourPassword();
 			checkPasswordValidation();
-		} else {if (password.length() < 8) {
-			System.out.println("Password is too short ");
-			enterYourPassword();
-			checkPasswordValidation();
 		} else {
-			if (password.length() > 12) {
-				System.out.println("Password is too long ");
+			if (password.length() < 8) {
+				System.out.println("Password is too short ");
 				enterYourPassword();
 				checkPasswordValidation();
 			} else {
-				if (!boolPassword) {
-					System.out.println("Password  is not strong enough ");
+				if (password.length() > 12) {
+					System.out.println("Password is too long ");
 					enterYourPassword();
 					checkPasswordValidation();
 				} else {
-					System.out.println();
+					if (boolPassword) {
+						System.out.println();
+					}
+					System.out.println("Password  is not strong enough ");
+					enterYourPassword();
+					checkPasswordValidation();
 				}
 			}
 		}
 	}
-}
 }
